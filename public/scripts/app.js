a1e02e27d13a61e11330dc6cc26c442101cdcdff
Srijith Kartha <ksrijith@gmail.com>
2018-05-16 08:04:09 +0000

    First Challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,56 @@
 "use strict";
 
 console.log("This is from app.js");
+
 var template = React.createElement(
-  "h1",
-  null,
-  "This is from jsx file"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "This is from jsx file"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "this is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
 );
+
 var app = document.getElementById("app");
-ReactDOM.render(template, app);
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Srijith Kartha"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 35"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Bengaluru"
+    )
+);
+
+ReactDOM.render(templateTwo, app);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
-"use strict";
-
 console.log("This is from app.js");
-var template = <h1>This is from jsx file</h1>;
+
+var template = (
+    <div>
+        <h1>This is from jsx file</h1>
+        <p>this is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
 var app = document.getElementById("app");
-ReactDOM.render(template, app);
+
+var templateTwo = (
+    <div>
+        <h1>Srijith Kartha</h1>
+        <p>Age: 35</p>
+        <p>Location: Bengaluru</p>
+    </div>
+);
+
+ReactDOM.render(templateTwo, app);
