1edafe88d17fd7d4054b4068e9bdbc8876e7cb61
Srijith Kartha <ksrijith@gmail.com>
2018-05-16 12:33:26 +0000

    Indecision App Render Template method

--- src/app.js
+++ src/app.js
@@ -4,23 +4,41 @@ console.log("This is from app.js");
 const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    options: [
-        'Item One',
-        'Item Two'
-    ]
+    options: []
 }
-const template = (
-    <div>
-        <h1>{ app.title }</h1>
-        { app.subtitle && <p>{ app.subtitle }</p> }
-        <p>{ app.options && app.options.length > 0?'Here are your options':'No options' }</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
+const submitForm = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if ( option ) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderTemplate();
+    }
+};
+
+
 // Root under which the JSX is rendered.
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+const renderTemplate = () => {
+    const template = (
+        <div>
+            <h1>{ app.title }</h1>
+            { app.subtitle && <p>{ app.subtitle }</p> }
+            <p>{ app.options && app.options.length > 0?'Here are your options':'No options' }</p>
+            <p>{ app.options.length }</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+
+            <form onSubmit={submitForm}>
+                <input type="text" name="option" />
+                <button type="submit">Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderTemplate();
