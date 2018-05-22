01f8280063ed1f7f3691b2babfaedf5be5b216dc
Srijith Kartha <ksrijith@gmail.com>
2018-05-22 05:33:43 +0000

    Basic Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// entry point is
+// final bundle js file
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+};
