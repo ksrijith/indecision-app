1b97901d93c645447932b1b5f4b01ad40382d206
Srijith Kartha <ksrijith@gmail.com>
2018-05-22 06:32:54 +0000

    Added babel and Loader in webpack

--- src/app.js
+++ src/app.js
@@ -1,20 +1,6 @@
-//import './utils.js'; 
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running');
-console.log('sq:' + square(4));
-console.log('add:' + add(4, 2));
-console.log('subtract:' + subtract(4, 2));
+const template = <p>testing 123</p>
 
-console.log('isAdult:' + isAdult(15));
-console.log('isAdult:' + isAdult(18));
-console.log('isAdult:' + isAdult(21));
-
-console.log('canDrink:' + canDrink(15));
-console.log('canDrink:' + canDrink(21));
-console.log('canDrink:' + canDrink(30));
-
-console.log('isSenior:' + isSenior(64));
-console.log('isSenior:' + isSenior(65));
-console.log('isSenior:' + isSenior(66));
+ReactDOM.render(template, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,17 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader'
+            }
+            }
+        ]
     }
 };
+
